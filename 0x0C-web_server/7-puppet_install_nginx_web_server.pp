35f5dcb0b05c5e18e1e7956d9cb712bbcd48257c
Evarmedia <mishakmanuel@gmail.com>
2024-01-29 09:56:00 +0100

    devops

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,7 +7,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/Evarmedia permanent;',
 }
 
 file { '/var/www/html/index.html':
