373411a98e34d5f1fca2acba0b817c5c765be3ef
Evarmedia <mishakmanuel@gmail.com>
2024-01-29 15:53:55 +0100

    puppetConf

--- 0x0F-load_balancer/2-puppet_custom_http_response_headerrr.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_headerrr.pp
@@ -3,7 +3,7 @@ package { 'haproxy':
   ensure => installed,
 }
 
-# custom configuration for HAProxy
+# Define custom configuration for HAProxy
 file { '/etc/haproxy/haproxy_custom.cfg':
   ensure  => present,
   content => @("EOF"
@@ -11,7 +11,7 @@ frontend http_front
     http-response set-header X-Served-By %H
 EOF
 ),
- require => Package['haproxy'],
+  require => Package['haproxy'],
   notify  => Service['haproxy'],
 }
 
@@ -31,31 +31,10 @@ file { '/etc/default/haproxy':
   notify  => Service['haproxy'],
 }
 
-# Update /etc/hosts with correct hostnames and IP addresses
-file { '/etc/hosts':
-  content => @("EOF"
-127.0.0.1 localhost
-100.26.168.235 150802-web-01
-18.207.234.92 150802-web-02
-# ... (other existing entries)
-EOF
-),
-  require => Package['haproxy'],
-}
-
-# Validate HAProxy configuration before restart
-exec { 'validate_haproxy_config':
-  command => 'haproxy -c -f /etc/haproxy/haproxy.cfg',
-  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
-  subscribe => File['/etc/haproxy/haproxy.cfg'],
-  notify  => Service['haproxy'],
-}
-
 # Restart HAProxy service
 service { 'haproxy':
   ensure    => running,
   enable    => true,
-  subscribe => [File['/etc/haproxy/haproxy_custom.cfg'], File['/etc/hosts'], Exec['include_custom_config'], Exec['validate_haproxy_config']],
+  subscribe => [File['/etc/haproxy/haproxy_custom.cfg'], Exec['include_custom_config']],
 }
 
