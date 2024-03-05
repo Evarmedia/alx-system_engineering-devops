7f44df6fd6eb43ae94db244d8626c40817d7ced7
Evarmedia <mishakmanuel@gmail.com>
2024-03-05 09:53:53 +0100

    script to query the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+''' a function that queries the Reddit API and
+returns the number of subscribers'''
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
