078f5723bd87d1a36a58183a3580db3b5def03cc
Evarmedia <mishakmanuel@gmail.com>
2024-03-05 09:56:06 +0100

    TopTen

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+import praw
+
+def top_ten(subreddit):
+'''TOPTEN'''
+    reddit = praw.Reddit(client_id='your_client_id',
+                         client_secret='your_client_secret',
+                         user_agent='your_user_agent')
+
+    try:
+        subreddit = reddit.subreddit(subreddit)
+        for submission in subreddit.hot(limit=10):
+            print(submission.title)
+    except prawcore.exceptions.Redirect:
+        print(None)
